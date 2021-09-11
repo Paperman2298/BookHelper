fd9d694d27467539868c31096241cc8a7a874930
Andruixxd31 <andruix93@gmail.com>
2021-09-11 15:45:25 -0500

    login and register activities modeled

---
+++ app/src/main/java/com/example/bookhelper/RegisterActivity.kt
@@ -0,0 +1,32 @@
+package com.example.bookhelper
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+class RegisterActivity : AppCompatActivity() {
+
+    lateinit var email : EditText
+    lateinit var password : EditText
+    lateinit var confirmedPassword : EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        email = findViewById(R.id.registerEmailEditText)
+        password = findViewById(R.id.registerPasswordEditText)
+        confirmedPassword = findViewById(R.id.registerConfirmPasswordEditText)
+    }
+
+    fun onRegisterClicked(view: View?){
+        //Create firebase user
+    }
+
+    fun onLoginClicked(view: View?){
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+}
