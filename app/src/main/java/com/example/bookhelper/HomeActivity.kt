929a4e59863f55a991986553815776f6a0f92273
Andruixxd31 <andruix93@gmail.com>
2021-09-20 15:06:10 -0500

    added cardview functionality

--- app/src/main/java/com/example/bookhelper/HomeActivity.kt
+++ app/src/main/java/com/example/bookhelper/HomeActivity.kt
@@ -2,11 +2,20 @@ package com.example.bookhelper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.supportActionBar?.hide()
         setContentView(R.layout.activity_home)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        val adapter = CustomAdapter()
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
     }
 
 
--- app/src/main/java/com/example/bookhelper/customAdapter.kt
+++ app/src/main/java/com/example/bookhelper/customAdapter.kt
@@ -1,2 +1,52 @@
 package com.example.bookhelper
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class CustomAdapter : RecyclerView.Adapter<CustomAdapter.ViewHolder>(){
+
+    val titles = arrayOf("A Brave New World", "Why I Write", "Man's Search For Meaning")
+    val authors = arrayOf("Aldous Huxley", "George Orwell", "Viktor E. Frankl")
+    val pages = arrayOf("197", "120", "165")
+    val images = intArrayOf(
+        R.drawable.ic_launcher_background,
+        R.drawable.ic_launcher_background,
+        R.drawable.ic_launcher_background,
+    )
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
+        val v = LayoutInflater.from(viewGroup.context).inflate(R.layout.card_layout, viewGroup, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
+        viewHolder.itemTitle.text = titles[i]
+        viewHolder.itemAuthor.text = authors[i]
+        viewHolder.itemPages.text = pages[i]
+        viewHolder.itemImage.setImageResource(images[i])
+    }
+
+    override fun getItemCount(): Int {
+        return titles.size
+    }
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        var itemImage: ImageView
+        var itemTitle: TextView
+        var itemAuthor : TextView
+        var itemPages : TextView
+
+        init {
+            itemImage = itemView.findViewById(R.id.item_image)
+            itemTitle = itemView.findViewById(R.id.item_title)
+            itemAuthor = itemView.findViewById(R.id.item_author)
+            itemPages = itemView.findViewById(R.id.item_pages)
+        }
+    }
+
+
+}
